408cba89b6d886d3453ddf4bed1a06f61c9df31a
George Wu <george@glooko.com>
2016-04-11 11:49:03 -0700

    test now returning boolean, screen shot next

--- src/test/java/Tests/GlookoLogbook/SignUpTests.java
+++ src/test/java/Tests/GlookoLogbook/SignUpTests.java
@@ -26,20 +26,27 @@ public class SignUpTests extends TestBase {
     public void setUpPage() {}
 
     @Test
-    public void signup1() throws Exception {
-        wait(for_radiobutton("QA"));
-        element(for_radiobutton("QA")).click();
-        wait(for_button("OK"));
-        element(for_button("OK")).click();
-        wait(for_id("com.glooko.logbook:id/sign_up"));
-        element(for_id("com.glooko.logbook:id/sign_up")).click();
-        wait(for_id("com.glooko.logbook:id/firstname_field"));
-        element(for_id("com.glooko.logbook:id/firstname_field")).sendKeys("TestFirstName");
-        element(for_id("com.glooko.logbook:id/lastname_field")).sendKeys("TestLastName");
-        String email = generateEmail();
-        element(for_id("com.glooko.logbook:id/username_field")).sendKeys(email);
-        element(for_id("com.glooko.logbook:id/sign_up_button")).click();
-        wait(for_text_exact("Are you compatible with Glooko?"));
+    public boolean signup1() throws Exception {
+        boolean passed = true;
+        try {
+            wait(for_radiobutton("QA"));
+            element(for_radiobutton("QA")).click();
+            wait(for_button("OK"));
+            element(for_button("OK")).click();
+            wait(for_id("com.glooko.logbook:id/sign_up"));
+            element(for_id("com.glooko.logbook:id/sign_up")).click();
+            wait(for_id("com.glooko.logbook:id/firstname_field"));
+            element(for_id("com.glooko.logbook:id/firstname_field")).sendKeys("TestFirstName");
+            element(for_id("com.glooko.logbook:id/lastname_field")).sendKeys("TestLastName");
+            String email = generateEmail();
+            element(for_id("com.glooko.logbook:id/username_field")).sendKeys(email);
+            element(for_id("com.glooko.logbook:id/sign_up_button")).click();
+            wait(for_text_exact("Are you compatible with Glooko?"));
+        } catch (Exception e) {
+            passed = false;
+        }
+        assert passed == true;
+        return passed;
     }
 
     /** wait wraps Helpers.wait **/
